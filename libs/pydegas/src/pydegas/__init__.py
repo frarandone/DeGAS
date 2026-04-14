1d4a95d838b7c105552792556f1ca32e4c69c65a
Oguzhan Merttürk <oguzmertturk@gmail.com>
2026-04-14 19:53:48 +0200

    add package level logging handler

--- libs/pydegas/src/pydegas/__init__.py
+++ libs/pydegas/src/pydegas/__init__.py
@@ -1,2 +1,5 @@
-def main() -> None:
-    print("Hello from pydegas!")
+import logging
+
+
+# prevent "No handlers could be found for logger 'pydegas'" warnings
+logging.getLogger(__name__).addHandler(logging.NullHandler())
